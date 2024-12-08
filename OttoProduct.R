721b13ea616cfb12ec2b168f88338037d87900b6
Brinley Ostler <brinley.ostler@gmail.com>
2024-12-07 17:10:23 -0700

    add final script

--- OttoProduct.R
+++ OttoProduct.R
@@ -5,6 +5,7 @@ library(vroom)
 library(lightgbm)
 library(bonsai)
 library(ranger)
+library(discrim)
 
 #### READ IN DATA ####
 otto_test = vroom("./test.csv")
@@ -13,7 +14,7 @@ otto_train = vroom("./train.csv")
 ## CLEAN THE DATA
 otto_train$target = factor(otto_train$target)
 
-
+#
 #### EDA ####
 glimpse(otto_train)
 #summary(otto_train)
@@ -26,128 +27,89 @@ otto_recipe <- recipe(target~., data=otto_train) %>%
   step_rm(id) %>% 
   step_normalize(all_numeric_predictors())
 
+#### LIGHT GBM ####
+otto_lgbm <- boost_tree(tree_depth = 4,  #4
+                       learn_rate = 0.1,  #0.1
+                       trees = 1000) %>% 
+  set_engine("lightgbm") %>% 
+  set_mode("classification")
+
+## CV 
+folds <- vfold_cv(otto_train, v=5, repeats=1)
+CV_results_lgbm <- fit_resamples(otto_lgbm,
+                                 otto_recipe,
+                                 folds,
+                                 metrics = metric_set(mn_log_loss),
+                                 control = control_resamples(save_pred = T))
+collect_metrics(CV_results_lgbm)
+# mean log loss: 0.491
+
 #### RANDOM FOREST ####
-otto_rf <- rand_forest(mtry = tune(),
-                       min_n = tune(),
+otto_rf <- rand_forest(mtry = 10,
+                       min_n = 2,
                        trees = 500) %>% 
   set_engine("ranger") %>% 
   set_mode("classification")
 
-## WORKFLOW
-rf_workflow <- workflow() %>% 
-  add_recipe(otto_recipe) %>% 
-  add_model(otto_rf)
-
-## Tuning Grid
-tuning_grid_rf <- grid_regular(mtry(range = c(1,10)),
-                            min_n(),
-                            levels=5)
-
-## CV 
-folds <- vfold_cv(otto_train, v=5, repeats=1)
-
 ## Run the CV
-CV_results_rf <- rf_workflow %>% 
-  tune_grid(resamples=folds,
-            grid=tuning_grid_rf,
-            metrics=metric_set(mn_log_loss))
-# metric_set(f_meas, sens, recall, spec)
+CV_results_rf <- fit_resamples(otto_rf,
+                               otto_recipe,
+                               folds,
+                               metrics = metric_set(mn_log_loss),
+                               control = control_resamples(save_pred = T))
+collect_metrics(CV_results_rf)
+# mean log loss: 0.567
 
-## Find best tuning parameters
-best_tune_rf <- CV_results_rf %>% 
-  select_best(metric="mn_log_loss")
-print(best_tune_rf)
-
-
-#### LIGHT GBM ####
-otto_lgbm <- boost_tree(tree_depth = tune(),  #15
-                       learn_rate = tune(),  #0.1
-                       trees = 500) %>% 
-  set_engine("lightgbm") %>% 
+#### NAIVE BAYES ####
+otto_nb <- naive_Bayes(Laplace=0,
+                       smoothness=1.5) %>%
+  set_engine("naivebayes") %>%
   set_mode("classification")
 
-## CV
-CV_results_lgbm <- rf_workflow %>% 
-  tune_grid(resamples=folds,
-            grid=tuning_grid_lgbm,
-            metrics=metric_set(mn_log_loss))
+CV_results_nb <- fit_resamples(otto_nb,
+                               otto_recipe,
+                               folds,
+                               metrics = metric_set(mn_log_loss),
+                               control = control_resamples(save_pred = T))
+collect_metrics(CV_results_nb)
+# mean log loss: 21.2
 
-## Find best tuning parameters
-best_tune_lgbm <- CV_results_lgbm %>% 
-  select_best(metric="mn_log_loss")
-print(best_tune_lgbm)
-
-## WORKFLOW
-#lgbm_workflow <- workflow() %>% 
-#  add_recipe(otto_recipe) %>% 
-#  add_model(otto_lgbm) %>% 
-#  fit(data = otto_train)
-
-
-#### XGBOOST ####
-otto_xgb <- boost_tree(learn_rate = tune(), #.3
-                       tree_depth = tune(), #6
-                       trees = 500) %>%
-  set_engine("xgboost") %>%
-  set_mode("classification")
-
-## WORKFLOW
-xgb_workflow <- workflow() %>% 
-  add_recipe(otto_recipe) %>% 
-  add_model(otto_xgb)
-
-## Tuning Grid
-tuning_grid_xgb <- grid_regular(learn_rate(),
-                                tree_depth(),
-                                levels=5)
-
-## CV
-CV_results_xgb <- xgb_workflow %>% 
-  tune_grid(resamples=folds,
-            grid=tuning_grid_xgb,
-            metrics=metric_set(mn_log_loss))
-
-best_tune_xgb <- CV_results_xgb %>% 
-  select_best(metric="mn_log_loss")
-print(best_tune_xgb)
 
 #### COMPARE OUTPUT ####
+collect_metrics(CV_results_lgbm) # best
 collect_metrics(CV_results_rf)
-collect_metrics(CV_results_xgb)
-collect_metrics(CV_results_lgbm)
+collect_metrics(CV_results_nb) # worst
 
 
 ######################################
 
 #### FINALIZE WF ####
-#final_wf <- rf_workflow %>% 
-#  finalize_workflow(best_tune) %>% 
-#  fit(data=otto_train)
+lgbm_workflow <- workflow() %>% 
+  add_recipe(otto_recipe) %>% 
+  add_model(otto_lgbm) %>% 
+  fit(data = otto_train)
 
 #### MAKE PREDICTIONS ####
-#otto_preds <- final_wf %>% 
-#  predict(new_data = otto_test, type="prob")
+otto_preds <- lgbm_workflow %>% 
+  predict(new_data = otto_test, type="prob")
 
 ## Format predictions for kaggle upload
-#otto_kaggle_submission <- otto_preds %>% 
-#  bind_cols(otto_te$id) %>%
-#  rename("id" = "...10",
-#         "Class_1"= ".pred_Class_1",
-#         "Class_2"= ".pred_Class_2",
-#         "Class_3"= ".pred_Class_3",
-#         "Class_4"= ".pred_Class_4",
-#         "Class_5"= ".pred_Class_5",
-#         "Class_6"= ".pred_Class_6",
-#         "Class_7"= ".pred_Class_7",
-#         "Class_8"= ".pred_Class_8",
-#         "Class_9"= ".pred_Class_9") %>%
-#  select(id, everything())
+otto_kaggle_submission <- otto_preds %>% 
+  bind_cols(otto_test$id) %>%
+  rename("id" = "...10",
+         "Class_1"= ".pred_Class_1",
+         "Class_2"= ".pred_Class_2",
+         "Class_3"= ".pred_Class_3",
+         "Class_4"= ".pred_Class_4",
+         "Class_5"= ".pred_Class_5",
+         "Class_6"= ".pred_Class_6",
+         "Class_7"= ".pred_Class_7",
+         "Class_8"= ".pred_Class_8",
+         "Class_9"= ".pred_Class_9") %>%
+  select(id, everything())
 
 ## Write out file
-#vroom_write(x=otto_kaggle_submission, file="./OttoClassPreds.csv", delim=",")
-
-
-
+vroom_write(x=otto_kaggle_submission, file="./OttoClassPreds.csv", delim=",")
 
 
 
